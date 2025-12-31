b8e8194c81fef49f2d2be06e2db5548afa35a511
AmanChauhan29 <iamanchauhan29@gmail.com>
2025-12-31 18:15:11 +0530

    email sending implementation done

--- routes/auth.py
+++ routes/auth.py
@@ -1,32 +1,51 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, BackgroundTasks
+from fastapi.responses import JSONResponse
 from models.user import UserCreate, UserOut, UserLogin
 from db.db_operation import mongo_conn
 from pymongo.errors import PyMongoError
+from utils.email import send_verification_email
 from utils.hash import verify_password
 from utils.jwt_handler import create_access_token
 from services.user_service import create_user
 from utils.logger import get_logger
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = get_logger("AUTH_ROUTE")
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
+FRONTEND_VERIFY_URL = os.getenv("FRONTEND_VERIFY_URL")
 
-@router.post("/signup", response_model=UserOut)
-async def signup(user: UserCreate):
+@router.post("/signup")
+async def signup(user: UserCreate, background_tasks: BackgroundTasks):
     logger.info(f"Attempting to sign up user with email: {user.email}")
     try:
-        await create_user(user)
-        logger.info(f"User created with email: {user.email}")
-        return {f"verification_email_sent_to_{user.email}"}
+        verify_token = await create_user(user)
+        verify_link = f"{FRONTEND_VERIFY_URL}?token={verify_token}"
+        # async email send
+        background_tasks.add_task(
+            send_verification_email,
+            user.email,
+            verify_link
+        )
+        logger.info(f"Soft User created with email: {user.email}")
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content={
+            "message": "User created successfully. Please check your email to verify your account.",
+            "email": user.email
+        })
     except ValueError as e:
         logger.error(f"Error during user signup: {e}")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except PyMongoError as e:
         logger.error(f"Database error during user signup: {e}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error")
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Database error")
     except Exception as e:
         logger.error(f"Unexpected error during user signup: {e}")   
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
     
 @router.post("/login")
 async def login(user: UserLogin):
@@ -36,9 +55,13 @@ async def login(user: UserLogin):
     if not db_user:
         logger.warning(f"Login failed: user not found {user.email}")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    if not user.get("is_verified"):
+        raise HTTPException(status_code=403, detail="Please verify your email before logging in")
+
     if db_user.get("disabled", False):
     # Reason: user account is administratively disabled
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Account disabled. Contact support.")
+    
     if not verify_password(user.password, db_user["password"]):
         logger.warning(f"Login failed: wrong password {user.email}")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
--- services/user_service.py
+++ services/user_service.py
@@ -1,9 +1,12 @@
+from fastapi import HTTPException, status
 from db.db_operation import mongo_conn
 from utils.hash import hash_password
 from models.user import UserCreate, UserOut
 from bson.objectid import ObjectId
 from utils.logger import get_logger
+from utils.token import create_email_verification_token
 from datetime import datetime
+
 
 logger = get_logger("USER_SERVICE")
 
@@ -11,8 +14,7 @@ async def create_user(user: UserCreate):
     logger.info(f"User create request received for email: {user.email}")
     users_collection = mongo_conn.users_collection
     if await users_collection.find_one({"email": user.email}):
-        logger.warning(f"Attempt to create a user with existing email: {user.email}")
-        raise ValueError("Email already registered")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already registered")
 
     user_dict = {
         "email": user.email,
@@ -29,4 +31,9 @@ async def create_user(user: UserCreate):
     }
     result =  await users_collection.insert_one(user_dict)
     logger.info(f"User inserted into database with id: {result.inserted_id}")
-    return 
+    verify_token = create_email_verification_token(user.email)
+    logger.info(
+        "Verification token generated",
+        extra={"email": user.email}
+    )
+    return verify_token
